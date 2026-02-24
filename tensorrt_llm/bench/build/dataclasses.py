304bbf2887e528a919f00ef147446650babf65c6
Fanrong Li <fanrongl@nvidia.com>
2026-02-24 00:53:42 -0800

    [None][fix] fix trtllm-bench to support mewtwo
    Signed-off-by: Fanrong Li <fanrongl@nvidia.com>

--- tensorrt_llm/bench/build/dataclasses.py
+++ tensorrt_llm/bench/build/dataclasses.py
@@ -183,6 +183,9 @@ class ModelConfig(BaseModel):
         """ Read the parameter count from HF safetensor metadata. """
         if model_hf_name == "EleutherAI/gpt-j-6b":  # GPT-J repo doesn't use safetensor format.
             param_count = 6053381344
+        # TODO: Remove this once HF supports Mewtwo
+        elif model_hf_name == "mewtwo/Mewtwo":
+            param_count = 284347051735
         else:
             model_name_or_path = hf_model_path or model_hf_name
             metadata = get_safetensors_metadata(model_name_or_path)
